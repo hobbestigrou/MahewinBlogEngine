8d9ff4bb2b52a34a5a9b3a294dd21e3a75befd47
Hobbestigrou <hobbestigrou@erakis.im>
2012-06-12 19:27:49 +0200

    [Core] Renderer: Better manage of renderer.

--- lib/MahewinBlogEngine/Renderer.pm
+++ lib/MahewinBlogEngine/Renderer.pm
@@ -3,10 +3,45 @@ package MahewinBlogEngine::Renderer;
 use Moose;
 use Carp;
 
-sub renderer {
-    my ( $self, $text ) = @_;
+use aliased 'MahewinBlogEngine::Renderer::Markdown';
+use aliased 'MahewinBlogEngine::Renderer::HTML';
+use Data::Dumper;
 
-    croak 'Not implemented';
+has _renderer_avalaible => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    lazy     => 1,
+    builder  => '_build_renderer_avalaible',
+    init_arg => undef
+);
+
+sub _build_renderer_avalaible {
+    my $rend = {
+        md   => sub {
+            my $renderer = Markdown->new;
+            $renderer->renderer(shift);
+        },
+        html => sub {
+            my $renderer = HTML->new();
+            $renderer->renderer(shift);
+        },
+    };
+
+    return $rend;
+};
+
+sub renderer {
+    my ( $self, $text, $format ) = @_;
+
+
+    if ( my $rend = $self->_renderer_avalaible->{$format} ) {
+        return $rend->($text);
+    }
+    else {
+        croak "No renderer for this format $format"
+    }
+
+    return;
 }
 
 1;
